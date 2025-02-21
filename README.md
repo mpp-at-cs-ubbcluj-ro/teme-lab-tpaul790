d4d2653af33d04c71bc6e4616fb848c15bfb3ebe
Titieni Paul <154063842+tpaul790@users.noreply.github.com>
2025-02-21 22:51:03 +0200

    Create lab1 module

--- README.md
+++
@@ -1,4 +0,0 @@
-# github-starting
-Starting with GitHub and IntelliJ
-
-Folosind IntelliJ, descarcati acest proiect pe calculatorul personal, adaugati o clasa  Java care tipareste un mesaj la consola si actualizati fisierele din repository-ul corespunzator.
--- lab1/src/main/java/org/example/lab1/Test.java
+++ lab1/src/main/java/org/example/lab1/Test.java
@@ -1,7 +1,7 @@
 package org.example.lab1;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Print something!");
+    public static void main(String[] args){
+        System.out.println("Print something for github");
     }
 }
